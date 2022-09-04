2ce267eacf9673c900384993a6c6c780b1078bbe
shubh-verma <shubhvcs@gmail.com>
2022-09-04 16:19:43 +0530

    final commit

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ let generateShop = () => {
 
       let search = basket.find((x) => x.id === id) || [];
       return `
-        <div id = product-id-${id}class="item">
+        <div id = product-id-${id} class="item">
                 <img width="220"src= ${img} alt="">
                 <div class="details">
                     <h3>${name}</h3>
@@ -18,9 +18,9 @@ let generateShop = () => {
                         <h2>$ ${price}</h2>
                         <div class="buttons">
                             <i onclick = "decrement(${id})" class="bi bi-dash-lg"></i>
-                            <div id = ${id} class="quantity">${
-        search.item === undefined ? 0 : search.item
-      }</div>
+                            <div id = ${id} class="quantity">
+                              ${search.item === undefined ? 0 : search.item}
+                            </div>
                             <i onclick = "increment(${id})" class="bi bi-plus-lg"></i>
                         </div>
                     </div>
