5dc883f38266a828994ff9c69946bc5648b81156
shubh-verma <shubhvcs@gmail.com>
2022-09-08 14:12:57 +0530

    react-demo init

---
+++ react-demo/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>To-do App will be here soon </h1>
+    </div>
+  );
+}
+
+export default App;
